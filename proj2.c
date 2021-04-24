20a2e48681ddea0793a3a98ae2237ea1f004a2a4
dyrkon <dyrkon603@gmail.com>
2021-04-24 21:11:50 +0200

    Include changes

---
+++ proj2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <libsync.h>
+#include <sys/shm.h>
+
+int main(void){
+
+    return 0;
+}
